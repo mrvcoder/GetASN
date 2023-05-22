19e0a2b08d2788c9cbd818f59b6c234dc1e58112
mrvcoder <sir.vcoder@gmail.com>
2023-05-22 22:49:20 +0330

    Update getasn.sh

--- getasn.sh
+++ getasn.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
 # Define the options that the script accepts
-options=":lhd"
+options=":h"
 dns="8.8.8.8"
 # Parse the options passed to the script
 while getopts "$options" opt; do
   case $opt in
-    l ) live_mode=1 ;;
-    h ) echo "usage: ./getasn.sh [options] ListOfUrls.txt "
-    echo "options:
-    -l => get all live ips of asns that find with this tool"
+    h ) echo "usage: ./getasn.sh [options] ListOfDomains.txt "
          exit 1
          ;;
     \? ) echo "Invalid option: -$OPTARG" 1>&2
@@ -19,35 +16,10 @@ while getopts "$options" opt; do
 done
 
 
-if [ "$live_mode" == "1" ]; then
-
-  # usage : getasn ListofUrls.txt
-  if [ -z "$2" ]; then
-    echo "Error: No input provided."
-    echo "usage: ./getasn.sh [options] ListOfUrls.txt "
-    echo "options:
-    -l => get all live ips of asns that find with this tool"
-    exit 1
-  fi
-
-
-  # Store the file path in a variable
-  input_file=$2
-
-  # Check if the file exists
-  if [ ! -f "$input_file" ]; then
-    echo "Error: File not found."
-    exit 1
-  fi
-
-else
-
- # usage : getasn ListofUrls.txt
+ # usage : getasn ListOfDomains.txt
   if [ -z "$1" ]; then
     echo "Error: No input provided."
-    echo "usage: ./getasn.sh [options] ListOfUrls.txt "
-    echo "options:
-    -l => get all live ips of asns that find with this tool"
+    echo "usage: ./getasn.sh [options] ListOfDomains.txt "
     exit 1
   fi
 
@@ -124,24 +96,3 @@ cat $output_file | jq .
 echo "========================="
 echo "Twitter: https://twitter.com/VC0D3R | Github : https://github.com/mrvcoder "
 echo -e "Done! \nOutPuts: $output_file - $output_file_same_asn - $output_file_not_cdn :)"
-
-# Check if live mode was enabled
-if [ "$live_mode" == "1" ]; then
-  # Loop through the ASNs in the file
-  # Get the unique ASN values from the JSON file
-  asns=$(cat output.json | jq '.urls[] | select(.is_cdn == "false") | .asn' | sort -u)
-  # Loop through each ASN value
-  for asn in $asns; do
-    # Run the desired command for each ASN
-    result=$(whois -h whois.radb.net -- "-i origin AS$asn" | grep -Eo "([0-9.]+){4}/[0-9]+" | uniq | mapcidr -silent | httpx)
-    echo "created file $asn.live.ips.txt"
-    echo $result > $asn.live.ips.txt
-  done
-  echo "========================="
-  echo "Done!" 
-  echo "========================="
-  echo "Please support :D"
-  echo "Twitter: https://twitter.com/VC0D3R | Github : https://github.com/mrvcoder"
-  echo "========================="
-
-fi

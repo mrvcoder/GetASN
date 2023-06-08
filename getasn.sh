a56aa5b81a30c2083dfce2424e071964dac854f8
mrvcoder <sir.vcoder@gmail.com>
2023-06-08 21:59:10 +0330

    Update getasn.sh

--- getasn.sh
+++ getasn.sh
@@ -1,21 +1,24 @@
 #!/bin/bash 
 
 # Define the options that the script accepts
-options=":shd:"
+options=":shr:d:"
 silent="0"
 dns="./resolvers.txt"
+opt_domain=""
 # Parse the options passed to the script
 while getopts "$options" opt; do
   case $opt in
     h ) 
          echo "usage: ./getasn.sh [options] ListOfDomains.txt"
-         echo " -d Set txt resolvers file address "
+         echo " -r Set resolvers file address "
         echo "-s silent output" 
         exit 1
          ;;
     s ) silent="1"
         ;;
-    d ) dns="$OPTARG"
+    r ) dns="$OPTARG"
+        ;;
+    d ) opt_domain="$OPTARG"
         ;;
     \? ) echo "Invalid option: -$OPTARG" 1>&2
          exit 1
@@ -24,14 +27,13 @@ while getopts "$options" opt; do
 done
 shift $(( OPTIND - 1 ))
 
- # usage : getasn ListOfDomains.txt
+if [ "$opt_domain" == "" ]; then
+   # usage : getasn ListOfDomains.txt
   if [ -z "$1" ]; then
     echo "Error: No input provided."
     echo "usage: ./getasn.sh [options] ListOfDomains.txt "
     exit 1
   fi
-
-
   # Store the file path in a variable
   input_file=$1
 
@@ -40,6 +42,10 @@ shift $(( OPTIND - 1 ))
     echo "Error: File not found."
     exit 1
   fi
+fi
+
+
+
 
 
 # Define the output file for the JSON object
@@ -63,27 +69,64 @@ fi
 # Initialize the JSON object
 json='{"domains":[]}'
 # update cut-cdn providers
-cut-cdn -ua
+cut-cdn -ua -silent
 
-# Loop through the URLs in the file
-api_reqs=0
-index=0
-while read -r domain; do
-  # Get the IP address of the URL
-  ips=$(echo $domain | dnsx -a -aaaa -resp-only -silent -r $dns -retry 3 -t 150)
-  for ip in $ips
-  do
-        # Check if the IP address is associated with a CDN
-        is_cdn=$(echo $ip | cut-cdn -silent -t 3 | wc -l)
+if [ "$opt_domain" = "" ]; then
+      while read -r domain; do
+        # Get the IP address of the URL
+        ips=$(echo $domain | dnsx -a -aaaa -resp-only -silent -r $dns -retry 3 -t 150)
+        for ip in $ips
+        do
+              # Check if the IP address is associated with a CDN
+              is_cdn=$(echo $ip | cut-cdn -silent -t 3 | wc -l)
+              ok=false
+              while [ "$ok" != "true" ]
+              do
+                  preflight=$(curl -v -s https://api.bgpview.io/ip/$ip  2>&1)
+                  httpResp=$(echo $preflight | grep -o -P '(HTTP/2 )[0-9]+')
+                  if [ "$httpResp" = "HTTP/2 200" ]; then
+                      sleep 20
+                     
+                      cidr=$(echo $preflight | sed 's/.*#0 to host api.bgpview.io left intact //' | jq -r ".data.prefixes[] | .prefix" -r | sort -u)
+                      asn=$(echo $preflight | sed 's/.*#0 to host api.bgpview.io left intact //'  | jq -r ".data.prefixes[] | .asn.asn" -r | sort -u)
+                      name=$(curl -s https://api.bgpview.io/asn/$asn | jq -r ".data .name" | sort -u)
+                      ok=true
+
+                      if [ $is_cdn == "0" ]
+                      then
+                            is_cdn=true
+                      else
+                            is_cdn=false
+                      fi
+
+
+
+                      # Append the information to the output file
+                      # Add the data to the JSON object
+                      json=$(echo $json | jq --arg domain "$domain" --arg ip "$ip" --arg asn "$asn" --arg is_cdn "$is_cdn" --arg cidr "$cidr" --arg name "$name" '.domains += [{"domain":$domain,"ip":$ip,"asn":$asn,"is_cdn":$is_cdn,"cidr":$cidr,"name":$name}]')
+                  
+                  else
+                    sleep 1m
+                  fi
+              done
+        done
+          sleep 5
+      done < "$input_file"
+else
+   # Check if the IP address is associated with a CDN
+    ips=$(echo $opt_domain | dnsx -a -aaaa -resp-only -silent -r $dns -retry 3 -t 150)
+    for ip in $ips
+    do
+        is_cdn=$(echo $ip | cut-cdn -silent | wc -l)
         ok=false
         while [ "$ok" != "true" ]
         do
-            preflight=$(curl -v -s https://api.bgpview.io/ip/$ip  2>&1 | grep -o -P '(HTTP/2 )[0-9]+')
-            if [ "$preflight" = "HTTP/2 200" ]; then
-                #echo "sleeping 20s"
+            preflight=$(curl -v -s https://api.bgpview.io/ip/$ip  2>&1)
+            httpResp=$(echo $preflight | grep -o -P '(HTTP/2 )[0-9]+')
+            if [ "$httpResp" = "HTTP/2 200" ]; then
                 sleep 20
-                cidr=$(curl -s https://api.bgpview.io/ip/$ip  2>&1 | jq -r ".data.prefixes[] | .prefix" -r | sort -u)
-                asn=$(curl -s https://api.bgpview.io/ip/$ip  | jq -r ".data.prefixes[] | .asn.asn" -r | sort -u)
+                cidr=$(echo $preflight | sed 's/.*#0 to host api.bgpview.io left intact //' | jq -r ".data.prefixes[] | .prefix" -r | sort -u)
+                asn=$(echo $preflight | sed 's/.*#0 to host api.bgpview.io left intact //' | jq -r ".data.prefixes[] | .asn.asn" -r | sort -u)
                 name=$(curl -s https://api.bgpview.io/asn/$asn | jq -r ".data .name" | sort -u)
                 ok=true
 
@@ -94,22 +137,15 @@ while read -r domain; do
                       is_cdn=false
                 fi
 
-
-
                 # Append the information to the output file
                 # Add the data to the JSON object
-                json=$(echo $json | jq --arg domain "$domain" --arg ip "$ip" --arg asn "$asn" --arg is_cdn "$is_cdn" --arg cidr "$cidr" --arg name "$name" '.domains += [{"domain":$domain,"ip":$ip,"asn":$asn,"is_cdn":$is_cdn,"cidr":$cidr,"name":$name}]')
-            
+                json=$(echo $json | jq --arg domain "$opt_domain" --arg ip "$ip"  --arg asn "$asn" --arg is_cdn "$is_cdn" --arg cidr "$cidr" --arg name "$name" '.domains += [{"domain":"'"$opt_domain"'","ip":$ip,"asn":$asn,"is_cdn":$is_cdn,"cidr":$cidr,"name":$name}]')
             else
-              #echo "sleeping 4m"
-              sleep 4m
+              sleep 1m
             fi
-        done
-  done
-	  #echo "sleeping 5s"
-  	sleep 5
-done < "$input_file"
-
+        done 
+    done
+fi
 
 
 # Write the JSON object to the output file
